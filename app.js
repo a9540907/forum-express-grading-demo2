13c04f87860ddb3117ba248c6d5e1156cc8012f7
stan_wang <a7569209@gmail.com>
2023-06-14 23:29:03 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const handlebars = require('express-handlebars')
 
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
+
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
-
 const db = require('./models')
+
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
