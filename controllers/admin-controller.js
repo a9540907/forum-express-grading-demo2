db5f4dae7a7a6f992720ae9c49869f83a6df0ad8
stan_wang <a7569209@gmail.com>
2023-07-02 22:26:50 +0800

    feat: show category on admin restaurant pages

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,11 +1,14 @@
-const { User } = require('../models')
-const { Restaurant } = require('../models')
+// const { User } = require('../models')
+// const { Restaurant } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
+const { Restaurant, User, Category } = require('../models')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category]
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
@@ -37,7 +40,9 @@ const adminController = {
   },
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
